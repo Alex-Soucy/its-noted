569b3cd8b5291cce070a2b0e8704420332410d25
Alex Soucy <asoucy22@yahoo.com>
2021-12-19 22:29:47 -0500

    added GET /notes function to go to notes.html

--- server.js
+++ server.js
@@ -2,10 +2,26 @@ const PORT = process.env.PORT || 3001;
 
 const fs = require('fs');
 
+const path = require('path');
+
 const express = require('express');
 
 const app = express();
 
+const completeNotes = require('./db/db.json');
+
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.use(express.static('public'));
+
+app.get('/api/notes', (req, res) => {
+    res.json(completeNotes.slice(1));
+});
+
+app.get('/notes', (req, res) => {
+    res.sendFile(path.join(__dirname, './public/notes.html'))
+})
 
 
 app.listen(PORT, () => {
