6a4b71e39fbafee2f5d9ac726ce7c2f8064612b2
Alex Soucy <asoucy22@yahoo.com>
2021-12-19 23:23:30 -0500

    added GET function for index.html as well as function to create notes and post them

--- server.js
+++ server.js
@@ -20,8 +20,36 @@ app.get('/api/notes', (req, res) => {
 });
 
 app.get('/notes', (req, res) => {
-    res.sendFile(path.join(__dirname, './public/notes.html'))
-})
+    res.sendFile(path.join(__dirname, './public/notes.html'));
+});
+
+app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, './public/index.html'));
+});
+
+function createNewNotes (body, createdNote) {
+    const newNote = body;
+    if (!Array.isArray(createdNote))
+        createdNote= [];
+
+    if(createdNote.length === 0)
+        createdNote.push(0);
+
+    body.id = createdNote[0];
+    createdNote[0]++;
+
+    createdNote.push(newNote);
+    fs.writeFileSync(
+        path.join(__dirname, './db/db.json'),
+        JSON.stringify(createdNote, null, 2)
+    );
+    return newNote;
+}
+
+app.post('/api/notes', (req, res) => {
+    const newNote = createNewNotes(req.body, completeNotes);
+    res.json(newNote);
+});
 
 
 app.listen(PORT, () => {
